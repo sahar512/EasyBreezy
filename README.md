97b0b5df66bf7ae8c0e871b9931f77802085725c
marian abosaleh <abosalehmarian@gmail.com>
2024-01-21 20:20:32 +0200

    react and npm start

--- README.md
+++
@@ -1,2 +0,0 @@
-# EasyBreezy
-shopping website
---
+++ easybreezy/src/Welcome/index.tsx
@@ -0,0 +1,20 @@
+// index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import classes from './style.css';
+
+function Welcome() {
+  return (
+    <div className={classes['welcome-container']}>
+      <div className={classes['easy-text']}>
+        EASY
+      </div>
+      
+      <div className={classes['breezy-text']}>
+        BREEZY
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
