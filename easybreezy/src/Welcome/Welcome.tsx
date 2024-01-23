c0da303a6c1fd65b555ce34abc3edc5f762541c3
marian abosaleh <abosalehmarian@gmail.com>
2024-01-23 14:05:17 +0200

    welcome1

---
+++ easybreezy/src/Welcome/Welcome.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import classes from './style.module.css';
+
+function Welcome() {
+  return (
+    <div className={classes['page-container']}>
+      <div className={classes['welcome-container']}>
+        <div className={classes['easy-text']}>
+          EASY
+        </div>
+        
+        <div className={classes['breezy-text']}>
+          BREEZY
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
